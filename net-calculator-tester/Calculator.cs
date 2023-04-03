2a0be0dd997a3b2438edb7346e88eec3267fabab
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-04-03 14:20:09 +0200

    Aggiungere i file di progetto.

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public static class Calculator
+    {
+        public static float Addizione(float num1, float num2)
+        {
+            float risultato = num1 + num2;
+
+            return risultato;
+        }
+
+        public static float Sottrazione(float num1, float num2)
+        {
+            float risultato = num1 - num2;
+
+            return risultato;
+        }
+
+        public static float Divisione(float num1, float num2)
+        {
+            float risultato = num1 / num2;
+
+            return risultato;
+        }
+
+        public static float Moltiplicazione(float num1, float num2)
+        {
+            float risultato = num1 * num2;
+
+            return risultato;
+        }
+    }
+}
